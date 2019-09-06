d726e5c97971b0193ba84b4ce72c83cd34d4b554
raylee1994 <849103991@qq.com>
2019-09-06 18:17:02 +0800

    modified

---
+++ src/routerGuard/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import {Route} from "react-router-dom";
+
+class RouterGuard extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <Route></Route>
+        )
+    }
+}
+
+export default RouterGuard
